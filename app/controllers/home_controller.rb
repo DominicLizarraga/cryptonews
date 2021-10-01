fece59404d28d0e6700ba23db50c81d1a9cc2196
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-09-30 21:25:31 -0700

    connecting api and presenting news

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
+    @uri = URI(@url)
+    @reponse = Net::HTTP.get(@uri)
+    @news = JSON.parse(@reponse)
   end
 
   def prices
