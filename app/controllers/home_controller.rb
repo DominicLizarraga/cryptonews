38e18634e8204ec94876841550a84aa47cb2bf11
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-09-30 22:12:29 -0700

    adding prices and table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,17 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-
+    # grab info news
     @url = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
     @uri = URI(@url)
     @reponse = Net::HTTP.get(@uri)
     @news = JSON.parse(@reponse)
+
+    # grab price data
+    @prices_url = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ADA,ETH,BCH,EOS,LTC,XLM,MIOTA,USDT,TRX&tsyms=USD"
+    @prices_uri = URI(@prices_url)
+    @prices_reponse = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_reponse)
   end
 
   def prices
